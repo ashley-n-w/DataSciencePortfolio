c8cc2a97159573ce46f36f67672eb91dde52d3b4
ashley-n-w <anwl8t@gmail.com>
2026-09-08 14:42:35 -0700

    add new function
    welcome_user function further personalizes the greeting by welcoming a user to whatever major program they choose.

--- Projects/greeting-project/hello.py
+++ Projects/greeting-project/hello.py
@@ -1,8 +1,14 @@
 # My first Python project in GitHub
 
 def greet_user():
-name = input("Enter your name: ")
-print(f"Hello, {name}! Welcome to Data Science.")
+  name = input("Enter your name: ")
+  print(f"\nHello, {name}!\n")
+
+def welcome_user():
+  major = input("Enter your major: ")
+  print(f"\nWelcome to {major} :)")
+  
 if __name__ == "__main__":
-print("Hi, my name is Ashley!")
-greet_user()
+  print("Hi, my name is Ashley!")
+  greet_user()
+  welcome_user()
